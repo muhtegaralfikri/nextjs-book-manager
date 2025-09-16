3a8ee39788967e9afa71e1d3cbebcf3e4780babd
muhtegaralfikri <105841110722@student.unismuh.ac.id>
2025-09-16 09:58:05 +0800

    refactor: clean up boilerplate from create-next-app

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+// src/app/page.tsx
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1 className="text-4xl font-bold">Sistem Manajemen Buku</h1>
+      {/* Nanti daftar buku akan muncul di sini */}
+    </main>
+  );
+}
